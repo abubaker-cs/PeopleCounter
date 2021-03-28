5f723b49b0ca085ba3cd7a797a8579c4723360aa
abubaker <mohammad_abubaker@yahoo.com>
2021-03-28 13:34:36 +0500

    Base setup

---
+++ app/src/main/java/org/abubaker/helloworld/MainActivity.kt
@@ -0,0 +1,25 @@
+package org.abubaker.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import org.abubaker.helloworld.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // setOnClickListener() for Button
+        binding.button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
