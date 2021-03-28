0cb8fe65483601d9be9d5fa21a20b217e9fedb4d
abubaker <mohammad_abubaker@yahoo.com>
2021-03-28 15:31:52 +0500

    Exercise completed using DataBinding

--- app/src/main/java/org/abubaker/helloworld/MainActivity.kt
+++ app/src/main/java/org/abubaker/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package org.abubaker.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.abubaker.helloworld.databinding.ActivityMainBinding
 
@@ -11,15 +12,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        var timesClicked = 9
+        // We will use it store current value for how many times a user clicked on the Button
+        var timesClicked = 0
 
         super.onCreate(savedInstanceState)
+
+        // We are asking to load our activity_main.xml layout as the initial view
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // setOnClickListener() for Button.
+        // This listener will initialize our custom code whenever the Button will be clicked
         binding.button.setOnClickListener {
-            // Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
+
+            // We are updating the value so the new clicked state can be stored
+            timesClicked += 1
+
+            /**
+             * Using DataBinding we are first selecting @+id/textView stored in our XML file and
+             * then updated its value with the newly updated timesClicked variable.
+             *
+             * Note:
+             * toString() will convert the integer value to string, so the textView can show it.
+             */
             binding.textView.text = timesClicked.toString()
+
+
+            // It will display a Toast message
+            Toast.makeText(
+                this@MainActivity,
+                "You clicked the button $timesClicked times.",
+                Toast.LENGTH_SHORT
+            ).show()
+
         }
 
     }
